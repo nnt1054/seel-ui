93c0142459603f7cb074498b3928cc0073b7ee35
Neil Toledo <neeull2@gmail.com>
2026-09-19 11:37:08 -0700

    adding installation and low fidelity diagram

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,9 @@ const config = {
     "../lib/**/*.mdx",
     "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+  "staticDirs": ['../public'],
   "addons": ["@storybook/addon-docs"],
   "framework": "@storybook/react-vite"
+
 };
 export default config;
